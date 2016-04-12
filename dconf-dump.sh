34998278468714d44fe694c314769fb073c920b3
Stefan Sonski <s.sonski@gmail.com>
2016-04-12 14:34:00 +0200

    Make dconfig dump and load script directory independent

--- dconf-dump.sh
+++ dconf-dump.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-dconf dump /org/gnome/terminal/ > config/dconf/dconf.d/terminal.conf
-dconf dump /org/gnome/GPaste/ > config/dconf/dconf.d/GPaste.conf
-dconf dump /org/gnome/desktop/ > config/dconf/dconf.d/desktop.conf
-dconf dump /org/gnome/gnome-session/ > config/dconf/dconf.d/session.conf
-dconf dump /org/gnome/nautilus/ > config/dconf/dconf.d/nautilus.conf
-dconf dump /org/gnome/shell/ > config/dconf/dconf.d/shell.conf
-dconf dump /org/gnome/settings-daemon/ > config/dconf/dconf.d/settings-daemon.conf
+set -e
+
+originalDirectory=`pwd`
+cd `dirname $0`
+directory=`pwd`
+cd $originalDirectory
+
+dconf dump /org/gnome/terminal/ > $directory/config/dconf/dconf.d/terminal.conf
+dconf dump /org/gnome/GPaste/ > $directory/config/dconf/dconf.d/GPaste.conf
+dconf dump /org/gnome/desktop/ > $directory/config/dconf/dconf.d/desktop.conf
+dconf dump /org/gnome/gnome-session/ > $directory/config/dconf/dconf.d/session.conf
+dconf dump /org/gnome/nautilus/ > $directory/config/dconf/dconf.d/nautilus.conf
+dconf dump /org/gnome/shell/ > $directory/config/dconf/dconf.d/shell.conf
+dconf dump /org/gnome/settings-daemon/ > $directory/config/dconf/dconf.d/settings-daemon.conf
--- dconf-load.sh
+++ dconf-load.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-cat config/dconf/dconf.d/terminal.conf | dconf load /org/gnome/terminal/
-cat config/dconf/dconf.d/GPaste.conf | dconf load /org/gnome/GPaste/
-cat config/dconf/dconf.d/desktop.conf | dconf load /org/gnome/desktop/
-cat config/dconf/dconf.d/session.conf | dconf load /org/gnome/gnome-session/
-cat config/dconf/dconf.d/nautilus.conf | dconf load /org/gnome/nautilus/
-cat config/dconf/dconf.d/shell.conf | dconf load /org/gnome/shell/
-cat config/dconf/dconf.d/settings-daemon.conf | dconf load /org/gnome/settings-daemon/
+set -e
+
+originalDirectory=`pwd`
+cd `dirname $0`
+directory=`pwd`
+cd $originalDirectory
+
+cat $directory/config/dconf/dconf.d/terminal.conf | dconf load /org/gnome/terminal/
+cat $directory/config/dconf/dconf.d/GPaste.conf | dconf load /org/gnome/GPaste/
+cat $directory/config/dconf/dconf.d/desktop.conf | dconf load /org/gnome/desktop/
+cat $directory/config/dconf/dconf.d/session.conf | dconf load /org/gnome/gnome-session/
+cat $directory/config/dconf/dconf.d/nautilus.conf | dconf load /org/gnome/nautilus/
+cat $directory/config/dconf/dconf.d/shell.conf | dconf load /org/gnome/shell/
+cat $directory/config/dconf/dconf.d/settings-daemon.conf | dconf load /org/gnome/settings-daemon/
